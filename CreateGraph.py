7d240b29786defc8a38e92d6180d95c92d907c68
David Chudzicki <dchudz@gmail.com>
2011-11-26 04:33:32 +0000

    Many more methods added to KnittedGraph class. Also added functions for flat, cylinder, negative curvature. Next step: finish function for decrease, maybe by first making "offsets" a list, then offset = [1,2]

--- CreateGraph.py
+++ CreateGraph.py
@@ -1,4 +1,5 @@
 from igraph import *
+from plotCN220Network3D import *
 
 class KnittedGraph(Graph):
     def __init__(self):
@@ -7,7 +8,7 @@ class KnittedGraph(Graph):
         self.current = int(0)
         self.direction = None
 
-    def AddStitch(self,offset = 1):
+    def AddStitch(self,offsets = 1):
         #add a stitch in same row
         #"offset" is offset in old row
         self.add_vertices(1)
@@ -50,8 +51,9 @@ class KnittedGraph(Graph):
         self.AddStitch()
         self.AddStitch(offset=0)
         
-#    def Decrease(self):        
+    def Decrease(self):        
         #add stitch as a decrease
+        
 
     def FinishRow(self):
         #knit until end of row
@@ -69,33 +71,67 @@ class KnittedGraph(Graph):
                 break
             self.AddStitch()
         
-        
+    def AtRowEnd(self):
+        #knit until end of row
+        offset = 1
+        DownList =  [e.source for e in self.es if e.target==self.current and e["type"]=="up"]
+        if len(DownList) > 0:
+            if self.direction == "opposite":
+                connection = min(DownList) - offset
+            elif self.direction == "same":
+                connection = max(DownList) + offset
+            else:
+                raise Exception("No good direction type")
+            if connection < 0 or self.vs[connection]["row"]  != self.vs[self.current]["row"] -1:
+                return True
+            else:
+                return False
 
+    def ConnectToZero(self):
+        #for circular knitting
+        self.direction = "same"
+        max_edge_id = g.ecount()
+        self.add_edges([(0,self.current)])        
+        g.es[max_edge_id]["type"] = "up"
+
+
+
+def FlatKnitting(RowLength, NumRows):
+    g = KnittedGraph()
+    g.AddStitches(RowLength-1)
+    for i in range(NumRows-1):
+        g.NewRow()
+        g.FinishRow()
+
+def NegCurvature(RowLength, NumStitches):
+    g = KnittedGraph()
+    g.AddStitches(RowLength-1)
+    g.NewRow()
+    for i in range(2000):
+        if g.AtRowEnd():
+            g.NewRow()
+        elif i % 3 == 0:
+            g.Increase()
+        else:
+            g.AddStitch()
 
 
 
 
 g = KnittedGraph()
-print g.get_edgelist()
-g.AddStitches(20)
-print g.get_edgelist()
-g.NewRow()
-#print g.get_edgelist()
-
-g.AddStitch()
-#g.AddStitch()
-#g.Increase()
-g.FinishRow()
-
-for i in range(30):
-    g.NewRow()
-    g.FinishRow()
-
-
+g.AddStitches(30)
+g.ConnectToZero()
+g.AddStitches(600)
 
 color_dict = {"across": "blue", "up": "red"}
 g.es["color"] = [color_dict[type] for type in g.es["type"]]
 
-#plot(g, "/home/ubuntu/dj/mysite/media/graphs/plot.png", layout=g.layout("kk") )
+plot(g, "/home/ubuntu/dj/mysite/media/graphs/plot2d.png", layout=g.layout("kk") )
 
-drawGraph3D(g, g.layout("kk_3d"), (1,2), "/home/ubuntu/dj/mysite/media/graphs/plot.png")
+
+
+#drawGraph3D(g, g.layout("kk_3d"), (1,2), "/home/ubuntu/dj/mysite/media/graphs/plot3d.png")
+layout = g.layout("kk_3d")
+directory = "/home/ubuntu/dj/mysite/media/graphs/cylinder/"
+drawGraphFrames(g, layout, directory,n=100)
+
